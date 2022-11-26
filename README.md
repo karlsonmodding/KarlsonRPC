610af7b8aa61c60340b86a2ce9ab2bc2ff9dd0eb
Dark42ed <tractoronfarm@gmail.com>
2022-11-26 15:04:31 -0500

    Rust rewrite

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# KarlsonRPC No-Mod Edition
+# KarlsonRPC
 ### Rich Presence for Karlson
 **Note: This version is (so far) free to use in runs submitted to speedrun.com**<br>
-![Example 1](https://i.ibb.co/RCyqdrp/image.png) ![Example 2](https://i.ibb.co/Hd7spjw/image.png)<br>
 
-## Requirements
-* .Net Framework 4.7.2 (or greater) Runtime
-* Microsoft Visual C++ 2015-2019 Redistributable
+## Usage
 
+1. Download the exe file from [releases](https://github.com/karlsonmodding/KarlsonRPC/releases)
+2. Run Karlson, and get to the main menu.
+3. Run KarlsonRPC.exe
 
-## Installation
+## Building
+With the rust toolchain installed:
+```
+git clone https://github.com/karlsonmodding/KarlsonRPC.git
+cd KarlsonRPC
+cargo build --release
+```
 
-1. Download the zip file from [releases](https://github.com/karlsonmodding/KarlsonRPC/releases) (Make sure to get the `.nomod` version!).
-This file contains KarlsonRPC, and a vanilla copy of Karlson. Make sure to leave everything in the same in the directory tree.
-2. Unzip
-3. Run KarlsonRPC.exe (This will automatically start Karlson)
 
 <hr>
 Big thanks to brododragon#9251 for the memory pointers!
---
+++ src/main.rs
@@ -0,0 +1,114 @@
+//UnityPlayer.dll+0x01683318,0x48,0x10,0x00
+use discord_rich_presence::{
+    activity::{Activity, Assets, Timestamps},
+    DiscordIpc, DiscordIpcClient,
+};
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
+use toy_arms::external::{read, Process};
+
+const LEVEL_MAP: &[(&str, usize)] = &[
+    ("Assets/Scenes/MainMenu.unity", 0),
+    ("Assets/Scenes/Stages/Escape/0Tut", 1),
+    ("Assets/Scenes/Stages/Sanbox/1San", 2),
+    ("Assets/Scenes/Stages/Sanbox/2San", 3),
+    ("Assets/Scenes/Stages/Sanbox/3San", 4),
+    ("Assets/Scenes/Stages/Escape/4Esc", 5),
+    ("Assets/Scenes/Stages/Escape/5Esc", 6),
+    ("Assets/Scenes/Stages/Escape/6Esc", 7),
+    ("Assets/Scenes/Stages/Escape/7Esc", 8),
+    ("Assets/Scenes/Stages/Sky/8Sky0.u", 9),
+    ("Assets/Scenes/Stages/Sky/9Sky1.u", 10),
+    ("Assets/Scenes/Stages/Sky/10Sky2.", 11),
+];
+
+const NAME_MAP: &[&str] = &[
+    "Main Menu",
+    "Tutorial",
+    "Sandbox 0",
+    "Sandbox 1",
+    "Sandbox 2",
+    "Escape 0",
+    "Escape 1",
+    "Escape 2",
+    "Escape 3",
+    "Sky 0",
+    "Sky 1",
+    "Sky 2",
+];
+
+const CLIENT_ID: &str = "798687396148281404";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        if let Ok(process) = Process::from_process_name("Karlson.exe") {
+            start_loop(process)?;
+        }
+        sleep(Duration::from_secs(1));
+    }
+
+    Ok(())
+}
+
+fn start_loop(process: Process) -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = DiscordIpcClient::new(CLIENT_ID).unwrap();
+    client.connect()?;
+    let mut last_level = usize::MAX;
+    let start_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)?
+        .as_secs() as i64;
+
+    println!("Start time is {}", start_time);
+    loop {
+        sleep(Duration::from_millis(100));
+        let ptr1 = read::<usize>(
+            process.process_handle,
+            process.get_module_base("UnityPlayer.dll").unwrap() + 0x01683318,
+        )?;
+        let ptr2 = read::<usize>(process.process_handle, ptr1 as usize + 0x48)?;
+        let ptr3 = read::<usize>(process.process_handle, ptr2 as usize + 0x10)?;
+        let val = read::<[u8; 32]>(process.process_handle, ptr3 as usize)?;
+        let string = core::str::from_utf8(&val)?;
+        let level = if &string[0..28] == "Assets/Scenes/MainMenu.unity" {
+            0
+        } else {
+            LEVEL_MAP.iter().find(|x| x.0 == string).unwrap().1
+        };
+
+        if last_level == level {
+            continue;
+        } else {
+            last_level = level;
+            println!("New level: {}", level);
+        }
+
+        let name = NAME_MAP[level];
+
+        let level_text = match level {
+            0 => "In Main Menu".to_string(),
+            x => format!("Playing {}", NAME_MAP[x]),
+        };
+
+        let assets = Assets::new()
+            .large_image("mainmenu")
+            .large_text("Main Menu");
+
+        let image_name = name.replace(" ", "").to_lowercase();
+        let assets = if level != 0 {
+            assets.small_image(&image_name).small_text(&name)
+        } else {
+            assets
+        };
+
+        client.set_activity(
+            Activity::new()
+                .timestamps(Timestamps::new().start(start_time))
+                .details(&level_text)
+                .assets(assets),
+        )?;
+    }
+
+    Ok(())
+}
+
+//UnityPlayer.dll+0x01683318,0x48,0x10,0x00
